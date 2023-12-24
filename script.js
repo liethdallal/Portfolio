c969f3f20aba404e1e44189d0f240a03b4b113dd
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-12-24 15:41:47 -0600

    added projects section

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ scrollToSection('about');
 scrollToSection('projects');
 scrollToSection('achievements');
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const projects = document.getElementById('projects');
+    const overlay = document.querySelector('.overlay');
+
+    projects.addEventListener('click', function (event) {
+        const projectsContainer = event.target.closest('.projects-container');
+        
+        if (projectsContainer) {
+            const projectId = projectsContainer.id;
+            const popupId = 'popup' + projectId.charAt(projectId.length - 1);
+
+            overlay.style.display = 'flex';
+
+            const popup = document.getElementById(popupId).cloneNode(true);
+            overlay.appendChild(popup);
+
+            popup.querySelector('.closeButton').addEventListener('click', function () {
+                overlay.style.display = 'none';
+                overlay.removeChild(popup);
+            });
+        }
+    });
+});
+
